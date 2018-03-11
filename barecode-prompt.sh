f958ae7744970bf12f90fabc9df1d0ddb6f3f27b
dcfvg <qwsx@dcfvg.com>
2018-03-11 10:10:51 +0000

    prototyping

---
+++ barecode-prompt.sh
@@ -0,0 +1,8 @@
+
+while [[ true ]]; do
+  echo "please scan a barcode!"
+  read varname
+  echo It\'s nice to meet you $varname
+  # lpr -P CUSTOM_Engineering_VKP80III -o media=X80MMYRoll -o fit-to-page output.pdf
+
+done
