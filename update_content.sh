6d141b106421567458872589d226923dda527f82
dcfvg <qwsx@dcfvg.com>
2018-05-01 11:27:31 +0200

    data url as param

--- update_content.sh
+++ update_content.sh
@@ -1,7 +1,9 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 cache=$DIR"/cache/pdf/"
 
-curl -L https://www.dropbox.com/sh/fsj4kx06eveyik1/AABVk8J-sYeV6S2LnJpAj_KJa?dl=1  >  pdf.zip
+source dropbox.sh
+
+curl -L $dropbox  >  pdf.zip
 rm -rf $cache
 unzip pdf.zip -d $cache
 rm pdf.zip
