ada2fd866ac7528624b406b0af640ddf7da1e3f3
dcfvg <qwsx@dcfvg.com>
2019-09-15 16:13:15 +0200

    default page

--- print-osc.js
+++ print-osc.js
@@ -1,5 +1,6 @@
 const { Server } = require('node-osc');
 const { exec } = require('child_process');
+const fs = require('fs');
 
 const oscServer = new Server(3333, '0.0.0.0');
 
@@ -12,20 +13,24 @@ oscServer.on('message', function (msg) {
   var dreamId = msg[2][1];
   var cache   = currentPath+"/cache/pdf/"
   var printer = "CUSTOM_Engineering_VKP80III"
+  var pdf = cache+dreamId+'.pdf':
 
-  var printOrder = 'lpr -P '+printer+' -#3 -o media=ZX80MMYRoll '+cache+dreamId+'.pdf';
+  // defaut page
+  if (!fs.existsSync(pdf)) var pdf = cache+'null.pdf':
 
-  console.log("SENDING", dreamId, printer);
+  var printOrder = 'lpr -P '+printer+' -#1 -o media=ZX80MMYRoll '+pdf;
+
+  console.log("SENDING", dreamId, printer, printOrder);
+
 
   exec(printOrder, (err, stdout, stderr) => {
     if (err) {
       console.error(err);
     } else {
-     // console.log(`stdout: ${stdout}`);
-     // console.log(`error: ${stderr}`);
+     //console.log(`stdout: ${stdout}`);
+     //console.log(`error: ${stderr}`);
      console.log(dreamId,"SENT");
     }
   });
-
   // oscServer.close();
 });
