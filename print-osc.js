5c5dc88c26427e8d37fb5e1a48d8e627896a685a
dcfvg <qwsx@dcfvg.com>
2019-09-21 17:38:25 +0200

    filtering osc message

--- print-osc.js
+++ print-osc.js
@@ -7,28 +7,34 @@ const oscServer = new Server(3333, '0.0.0.0');
 oscServer.on('message', function (msg) {
   // console.log(`Message: ${msg}`);
   console.log(msg[2]);
+  var requestUrl  = msg[2][0];
 
-  var currentPath = process.cwd();
+  // check URI
+  if(requestUrl == '/printdream'){
 
-  var dreamId = msg[2][1];
-  var cache   = currentPath+"/cache/pdf/";
-  var printer = "CUSTOM_Engineering_VKP80III";
-  var pdf = cache+dreamId+'.pdf';
+    var dreamId = msg[2][1];
+    var currentPath = process.cwd();
 
-  // defaut page
-  if (!fs.existsSync(pdf)) var pdf = cache+'null.pdf';
+    var cache   = currentPath+"/cache/pdf/";
+    var printer = "CUSTOM_Engineering_VKP80III";
+    var pdf = cache+dreamId+'.pdf';
 
-  var printOrder = 'lpr -P '+printer+' -#1 -o media=ZX80MMYRoll '+pdf;
-  console.log("SENDING", dreamId, printer, printOrder);
+    // defaut page
+    if (!fs.existsSync(pdf)) var pdf = cache+'null.pdf';
 
-  exec(printOrder, (err, stdout, stderr) => {
-    if (err) {
-      console.error(err);
-    } else {
-     //console.log(`stdout: ${stdout}`);
-     //console.log(`error: ${stderr}`);
-     console.log(dreamId,"SENT");
-    }
-  });
+    var printOrder = 'lpr -P '+printer+' -#1 -o media=ZX80MMYRoll '+pdf;
+    console.log("SENDING", dreamId, printer, printOrder);
+
+    exec(printOrder, (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+      } else {
+       //console.log(`stdout: ${stdout}`);
+       //console.log(`error: ${stderr}`);
+       console.log(dreamId,"SENT");
+      }
+    });
+  }
+
   // oscServer.close();
 });
